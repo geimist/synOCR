e339e13116cf66680f1e8b4579cef680b22e031c
Stephan Geisler <synotr@geimist.eu>
2019-01-22 20:06:06 +0000

    „Build/upgradeconfig.sh“ ändern

--- Build/upgradeconfig.sh
+++ Build/upgradeconfig.sh
@@ -4,10 +4,10 @@
 
 # Arbeitsverzeichnis auslesen und hineinwechseln:
 # ---------------------------------------------------------------------
-	APPDIR=$(cd $(dirname $0);pwd)
-	cd ${APPDIR}
-	
-	CONFIG=etc/Konfiguration.txt
+    APPDIR=$(cd $(dirname $0);pwd)
+    cd ${APPDIR}
+
+    CONFIG=etc/Konfiguration.txt
 
     lastrow=`cat ./$CONFIG | tail -n1`  # letzte Zeile eine Leerzeile?
 #    [ ! "$lastrow" == "" ] && echo -e "\n" >> ./$CONFIG
@@ -15,20 +15,20 @@
     if [ ! -z "$lastrow" ]; then
         echo -e "\n" >> ./$CONFIG
     fi
-    
+
     # Prüfe die Konfiguration.txt auf fehlende Parameter:
     # ---------------------------------------------------------------------
-    	if ! cat ./$CONFIG | grep -q "delSearchPraefix" ; then
-    	    echo "delSearchPraefix=\"no\"" >> ./$CONFIG
+        if ! cat ./$CONFIG | grep -q "delSearchPraefix" ; then
+            echo "delSearchPraefix=\"no\"" >> ./$CONFIG
         fi
-    	if ! cat ./$CONFIG | grep -q "NameSyntax" ; then
-    	    echo "NewName=\"\"" >> ./$CONFIG
+        if ! cat ./$CONFIG | grep -q "NameSyntax" ; then
+            echo "NewName=\"\"" >> ./$CONFIG
         fi
-    	if ! cat ./$CONFIG | grep -q "taglist" ; then
-    	    echo "taglist=\"\"" >> ./$CONFIG
+        if ! cat ./$CONFIG | grep -q "taglist" ; then
+            echo "taglist=\"\"" >> ./$CONFIG
         fi
-    	if ! cat ./$CONFIG | grep -q "moveTaggedFiles" ; then
-    	    echo "moveTaggedFiles=\"no\"" >> ./$CONFIG
+        if ! cat ./$CONFIG | grep -q "moveTaggedFiles" ; then
+            echo "moveTaggedFiles=\"no\"" >> ./$CONFIG
         fi
 
 exit 0
