2488fcab7e337638fcf7d287425911d860d8ecb1
Stephan Geisler <synotr@geimist.eu>
2019-01-27 12:44:30 +0000

    „build_spk.sh“ ändern

--- build_spk.sh
+++ build_spk.sh
@@ -17,7 +17,6 @@
 
 project="synOCR"
 
-
 skriptuser=`whoami`
 if [ ${skriptuser} != "root" ]; then
     echo "Dieses Skript muss von Root ausgeführt werden!"
@@ -31,6 +30,18 @@ cd ${APPDIR}
 
 build_tmp="${APPDIR}/build_tmp"
 dir=${APPDIR}
+buildversion=$1
+
+
+
+
+# ACHTUNG: derzeit wird ein bestehendes Projektverzeichnis gelöscht und mit dem Serververzeichnis ersetzt!
+	if [ -d "./${project}" ] ; then
+		rm -rf "./${project}"
+	fi
+
+
+
 
 gitpull() 
 {
@@ -57,24 +68,28 @@ if [ -d "./${project}" ] ; then
     cd ${APPDIR}
 else
     git clone https://geimist.eu:30443/geimist/${project}.git
+    cd ${project}
+    git pull
+    versions=`git tag`
+    cd ${APPDIR}
 fi
 
 
-build_version=`cat "${APPDIR}/${project}/Pack/INFO" | grep version | awk -F '"' '{print $2}'`
+#build_version=`cat "${APPDIR}/${project}/Pack/INFO" | grep version | awk -F '"' '{print $2}'`
 
 # welche Version soll gebaut werden:
-if [ -z $1 ]; then
+if [ -z $buildversion ]; then
      echo "git checkout zu master-branch"
     cd ${project}
     git checkout master
     cd ${APPDIR}
     set_spk_version="latest_(`date +%Y`-`date +%m`-`date +%d`_`date +%H`-`date +%M`)"
 else
-    if echo "$versions" | egrep -q "$1"; then
-        echo "git checkout zu $1"
+    if echo "$versions" | egrep -q "$buildversion"; then
+        echo "git checkout zu $buildversion"
         cd ${project}
-        git checkout "$1"
-        set_spk_version="$1"
+        git checkout "$buildversion"
+        set_spk_version="$buildversion"
         cd ${APPDIR}
     else
         echo "ACHTUNG: Die gewünschte Version wurde im Repository nicht gefunden!"
@@ -87,11 +102,10 @@ else
 fi
 }
 
-#    gitpull
+gitpull
 
-
-build_version=`cat "${APPDIR}/${project}/Pack/INFO" | grep version | awk -F '"' '{print $2}'`
-set_spk_version=$build_version
+    build_version=`cat "${APPDIR}/${project}/Pack/INFO" | grep version | awk -F '"' '{print $2}'`
+#    set_spk_version=$build_version
 
 echo " - INFO: Es wird foldende Version geladen und gebaut: $set_spk_version - BUILD-Version (INFO-File): $build_version"
 
@@ -121,14 +135,14 @@ cp -r "${APPDIR}/${project}"/* "${build_tmp}/"
 	echo " - INFO: Dateirechte anpassen ..."
 	for i in $(find "${build_tmp}/Pack/" -type f)
         do
-            echo "ändere Pack: $i"
+        #    echo "ändere Pack: $i"
             chmod 755 "$i"
             chown root:root "$i"
         done
 	
 	for i in $(find "${build_tmp}/Build/" -type f)
         do
-            echo "ändere Build: $i"
+        #    echo "ändere Build: $i"
             chmod 755 "$i"
             chown root:root "$i"
         done
