1d5a348ae7542d3ed31d16ddc0daccf1117d5f98
Stephan Geisler <synotr@geimist.eu>
2019-01-31 12:38:28 +0000

    „Build/synOCR-start.sh“ ändern

--- Build/synOCR-start.sh
+++ Build/synOCR-start.sh
@@ -2,6 +2,9 @@
 # /usr/syno/synoman/webman/3rdparty/synOCR/synOCR-start.sh
 # wechselt in synOCR-Verzeichnis und startet synOCR mit bzw. ohne LOG (je nach Konfiguration)
 
+# wurde das Skript von der GUI aufgerufen?
+    callFrom=$1
+    
 # Arbeitsverzeichnis auslesen und hineinwechseln:
     APPDIR=$(cd $(dirname $0);pwd)
     cd ${APPDIR}
@@ -11,45 +14,72 @@
     . ./$CONFIG
 
     LOGDIR="${LOGDIR%/}/"
-
     LOGFILE="${LOGDIR}synOCR_`date +%Y`-`date +%m`-`date +%d`_`date +%H`-`date +%M`.log"
 
-    synOCR_pid=`pidof synOCR.sh`
+    synOCR_pid=$( pidof synOCR.sh )
 
     if [ ! -z "$synOCR_pid" ] ; then
-        echo '<p class="center"><span style="color: #BD0010;"><b>synOCR läuft bereits!</b><br>(Prozess-ID: '$synOCR_pid')</span></p>'
-        echo '<br /><p class="center"><button name="page" value="status-kill-synocr" style="color: #BD0010;">(Beenden erzwingen?)</button></p><br />'
+        if [ $callFrom = GUI ] ; then
+            echo '<p class="center"><span style="color: #BD0010;"><b>synOCR läuft bereits!</b><br>(Prozess-ID: '$synOCR_pid')</span></p>'
+            echo '<br /><p class="center"><button name="page" value="status-kill-synocr" style="color: #BD0010;">(Beenden erzwingen?)</button></p><br />'
+        else
+            echo "synOCR läuft bereits! (Prozess-ID: ${synOCR_pid})"
+        fi
         exit
     else
-        echo '<p class="title">synOCR wurde gestartet ...</p><br><br><br><br>
-        <center><table id="system_msg" style="width: 40%;table-align: center;">
-            <tr>   
-                <th style="width: 20%;"><img class="imageStyle" alt="status_loading" src="images/status_loading.gif" style="float:left;"></th>   
-                <th style="width: 80%;"><p class="center"><span style="color: #424242;font-weight:normal;">Bitte warten, bis die Dateien<br>fertig abgearbeitet wurden.</span></p></th>
-            </tr>
-        </table></center>'   
+        if [ $callFrom = GUI ] ; then
+            echo '<p class="title">synOCR wurde gestartet ...</p><br><br><br><br>
+            <center><table id="system_msg" style="width: 40%;table-align: center;">
+                <tr>   
+                    <th style="width: 20%;"><img class="imageStyle" alt="status_loading" src="images/status_loading.gif" style="float:left;"></th>   
+                    <th style="width: 80%;"><p class="center"><span style="color: #424242;font-weight:normal;">Bitte warten, bis die Dateien<br>fertig abgearbeitet wurden.</span></p></th>
+                </tr>
+            </table></center>'
+        else
+            echo "synOCR wurde gestartet ..."
+            echo "Bitte warten, bis die Dateien fertig abgearbeitet wurden."
+        fi
     fi
     
     if [ ! -d "${INPUTDIR}" ] || ! $(echo "${INPUTDIR}" | grep -q "/volume") ; then
-        echo '
-        <p class="center"><span style="color: #BD0010;"><b>! ! ! Quellverzeichnis in der Konfiguration prüfen ! ! !</b><br>Programmlauf wird beendet.<br></span></p>'
+        if [ $callFrom = GUI ] ; then
+            echo '
+            <p class="center"><span style="color: #BD0010;"><b>! ! ! Quellverzeichnis in der Konfiguration prüfen ! ! !</b><br>Programmlauf wird beendet.<br></span></p>'
+        else
+            echo "! ! ! Quellverzeichnis in der Konfiguration prüfen ! ! !"
+            echo "Programmlauf wird beendet."
+        fi
         exit 1
     fi
     
     if [ ! -d "$OUTPUTDIR" ] && echo "$OUTPUTDIR" | grep -q "/volume" ; then
         mkdir -p "$OUTPUTDIR"
-        echo '
-        <p class="center"><span style="color: #BD0010;"><b>Zielverzeichnis wurde erstellt.</b></span></p>'
+        if [ $callFrom = GUI ] ; then
+            echo '
+            <p class="center"><span style="color: #BD0010;"><b>Zielverzeichnis wurde erstellt.</b></span></p>'
+        else
+            echo "Zielverzeichnis wurde erstellt."
+        fi
     elif [ ! -d "$OUTPUTDIR" ] || ! $(echo "$BACKUPDIR" | grep -q "/volume") ; then
-        echo '
-        <p class="center"><span style="color: #BD0010;"><b>! ! ! Zielverzeichnis in der Konfiguration prüfen ! ! !</b><br>Programmlauf wird beendet.<br></span></p>'
+        if [ $callFrom = GUI ] ; then
+            echo '
+            <p class="center"><span style="color: #BD0010;"><b>! ! ! Zielverzeichnis in der Konfiguration prüfen ! ! !</b><br>Programmlauf wird beendet.<br></span></p>'
+        else
+            echo "! ! ! Zielverzeichnis in der Konfiguration prüfen ! ! !"
+            echo "Programmlauf wird beendet."
+        fi
         exit 1
     fi
 
-	# nur starten (LOG erstellen), sofern es etwas zu tun gibt:
+    # nur starten (LOG erstellen), sofern es etwas zu tun gibt:
     if [ $( ls -t "${INPUTDIR}" | egrep -oi "${SearchPraefix}.*.pdf$" | wc -l ) = 0 ] ;then
-        echo '
-        <p class="center"><span style="color: #228b22;"><b>es gibt nichts zu tun</b><br>Programmlauf wird beendet.<br></span></p>'
+        if [ $callFrom = GUI ] ; then
+            echo '
+            <p class="center"><span style="color: #228b22;"><b>es gibt nichts zu tun</b><br>Programmlauf wird beendet.<br></span></p>'
+        else
+            echo "es gibt nichts zu tun"
+            echo "Programmlauf wird beendet."
+        fi
         exit 0
     fi
 
