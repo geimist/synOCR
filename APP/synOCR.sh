75a943cc9133870065de2b7e4b2bd1926b11d297
Stephan Geisler <synotr@geimist.eu>
2019-03-04 21:28:19 +0000

    „APP/synOCR.sh“ ändern

--- APP/synOCR.sh
+++ APP/synOCR.sh
@@ -176,6 +176,10 @@ IFS=$'\012'	 # entspricht einem $'\n' Newline
 for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -type f) #  -mmin +"$timediff" -o -name "${SearchPraefix}*.PDF" 
     do
         IFS=$OLDIFS
+    # temporäres Arbeitsverzeichnis erstellen
+        work_tmp=$(mktemp -d -t tmp.XXXXXXXXXX)
+        trap 'rm -rf "$work_tmp"; exit' EXIT
+        
         echo -e
         filename=$(basename "$input")
         title=${filename%.*}
@@ -183,23 +187,10 @@ for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -ty
         echo " ($(date))"
         date_start=$(date +%s)
 
-    # Zieldateiname erstellen (berücksichtigt gleichnamige vorhandene Dateien):
         if [ $delSearchPraefix = "yes" ] && [ ! -z "${SearchPraefix}" ]; then
             title=$( echo "${title}" | sed s/${SearchPraefix}//I )
         fi
-
-        destfilecount=$(ls -t "${OUTPUTDIR}" | grep -o "^${title}.*" | wc -l)
-        if [ $destfilecount -eq 0 ]; then
-            output="${OUTPUTDIR}${title}.pdf"
-        else
-            while [ -f "${OUTPUTDIR}${title} ($destfilecount).pdf" ]
-                do
-                    destfilecount=$( expr $destfilecount + 1 )
-                    echo "                          zähle weiter … ($destfilecount)"
-                done
-            output="${OUTPUTDIR}${title} ($destfilecount).pdf"
-            echo "                          Dateiname ist bereits vorhanden! Ergänze Zähler ($destfilecount)"
-        fi
+        output="${work_tmp}/${title}.pdf"
         echo "                          temp. Zieldatei: ${output}"
 
     # OCRmyPDF:
@@ -264,32 +255,16 @@ for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -ty
         fi
 
     # Datei-Attripute übertragen:
-        echo "                      --> übertrage die Dateirechte und -besitzer"
-    #    cp --attributes-only -p "$input" "$output"
-        
-    # Dateirechte-Log:
-        if [ $loglevel = "2" ] ; then
-            echo "                      --> Dateirechte Zieldatei:"
-            echo -n "                          "
-            ls -l "$output"
-        fi
-        
-        
-        
+        # Probleme bei ACL-Berechtigungen …
+        # echo "                      --> übertrage die Dateirechte und -besitzer"
+        # cp --attributes-only -p "$input" "$output"
+
     # suche nach Datum und Tags in Dokument:
         findDate()
         {
-#        if [ ! umbenennung aktiv? ]; then
-#            return
-#        fi
-
         # Text exrahieren
-            if [ -d "/tmp/synOCR" ]; then
-                rm -r /tmp/synOCR
-            fi
-            searchfile="/tmp/synOCR/synOCR.txt" # evtl. nur in Variable schreiben
-            mkdir /tmp/synOCR
-            
+            searchfile="${work_tmp}/synOCR.txt"
+
             if [ $searchAll = no ]; then
                 pdftotextOpt="-l 1"
             else
@@ -417,10 +392,9 @@ for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -ty
                     date_dd=$(printf '%02d' $(( 10#$(echo $founddate | awk -F'[./-]' '{print $2}' | grep -o '[0-9]*') )))
                     date_mm=$(printf '%02d' $(( 10#$(echo $founddate | awk -F'[./-]' '{print $1}') )))
 
-# 386 
+# hier stimmt etwas noch nicht mit der RegEx fürs Datum:
 #      prüfe Datum: [72.07.41 --> ungültiges Format
 #      prüfe Datum: [72.07.41./synOCR.sh: line 386: 10#[72 : syntax error: invalid arithmetic operator (error token is "[72 ") --> ungültiges Format
-
 
                     date_yy=$(echo $founddate | awk -F'[./-]' '{print $3}' | grep -o '[0-9]*')    
                     if [ $(echo $date_yy | wc -c) -eq 3 ] ; then
@@ -451,14 +425,6 @@ for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -ty
             fi
         }
         findDate
-
-        
-    # Dateirechte-Log:
-        if [ $loglevel = "2" ] ; then
-            echo "                      --> Dateirechte Zieldatei:"
-            echo -n "                          "
-            ls -l "$output"
-        fi
         
     # Dateinamen zusammenstellen und umbenennen:
         rename() 
@@ -520,8 +486,8 @@ for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -ty
                     DestFolderList="${tagarray[$i]}\n${DestFolderList}"
                     i=$((i + 1))
                 done
-                echo "                      --> lösche temp. Zieldatei"
-                rm "${outputtmp}"
+#                echo "                      --> lösche temp. Zieldatei"
+#                rm "${outputtmp}"
             elif [ ! -z "$renameTag" ] && [ $moveTaggedFiles = useTagDir ] ; then
                 echo "                      --> verschiebe in Tagverzeichnisse"
                 renameTag=$( echo $renameTag | sed -e "s/#//g" )
@@ -558,8 +524,8 @@ for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -ty
                     cp -l "${outputtmp}" "${output}"
                     i=$((i + 1))
                 done
-                echo "                      --> lösche temp. Zieldatei"
-                rm "${outputtmp}"
+#                echo "                      --> lösche temp. Zieldatei"
+#                rm "${outputtmp}"
             else
                 destfilecount=$(ls -t "${OUTPUTDIR}" | grep -o "^${NewName}.*" | wc -l)
                 if [ $destfilecount -eq 0 ]; then
@@ -573,9 +539,7 @@ for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -ty
                     output="${OUTPUTDIR}${NewName} ($destfilecount).pdf"
                     echo "                          Dateiname bereits vorhanden! Ergänze Zähler ($destfilecount)"
                 fi
-                echo "                          Umbenennung ursprüngliche Zieldatei"
-                echo "                          von:    $(basename "${outputtmp}")"
-                echo "                          nach:   $(basename "${output}")"
+                echo "                          Zieldatei: $(basename "${output}")"
                 mv "${outputtmp}" "${output}"
             fi
         fi
@@ -633,7 +597,9 @@ for input in $(find "${INPUTDIR}" -maxdepth 1 -iname "${SearchPraefix}*.pdf" -ty
         synosetkeyvalue ./etc/counter pagecount $(expr $(get_key_value ./etc/counter pagecount) + $pagecount_latest)
         synosetkeyvalue ./etc/counter ocrcount $(expr $(get_key_value ./etc/counter ocrcount) + 1)
         echo "                          INFO: (Laufzeit letzte Datei: $(( $(date +%s) - $date_start )) Sekunden (Seitenanzahl: $pagecount_latest) | gesamt: $(get_key_value ./etc/counter ocrcount) PDFs / > $(get_key_value ./etc/counter pagecount) Seiten bisher verarbeitet)"
-        # wget --timeout=30 --tries=2 -q -O - "http://${synocrdomain}/synOCR/synOCR_FILECOUNT" >/dev/null 2>&1
+
+    # temporäres Arbeitsverzeichnis löschen:
+        rm -rf "$work_tmp"
     done
 }
 
