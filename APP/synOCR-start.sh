17db6931720a8cb6e846598666820a26b63c1e40
Stephan Geisler <synotr@geimist.eu>
2019-03-12 17:34:45 +0000

    „APP/synOCR-start.sh“ ändern

--- APP/synOCR-start.sh
+++ APP/synOCR-start.sh
@@ -13,128 +13,141 @@
     cd ${APPDIR}
 
 # Konfigurationsdatei einbinden:
-    CONFIG=etc/Konfiguration.txt
-    . ./$CONFIG
+    #    CONFIG=etc/Konfiguration.txt
+    #    . ./$CONFIG
+    sSQL="SELECT profile_ID, INPUTDIR, OUTPUTDIR, LOGDIR, SearchPraefix, loglevel FROM config WHERE active='1' "
+    sqlerg=`sqlite3 -separator $'\t' ./etc/synOCR.sqlite "$sSQL"`
 
-# läuft bereits eine Instanz von synOCR?
-    synOCR_pid=$( pidof synOCR.sh )
-    if [ ! -z "$synOCR_pid" ] ; then
-        if [ $callFrom = GUI ] ; then
-            echo '<p class="center"><span style="color: #BD0010;"><b>synOCR läuft bereits!</b><br>(Prozess-ID: '$synOCR_pid')</span></p>'
-            echo '<br /><p class="center"><button name="page" value="status-kill-synocr" style="color: #BD0010;">(Beenden erzwingen?)</button></p><br />'
-        else
-            echo "synOCR läuft bereits! (Prozess-ID: ${synOCR_pid})"
-        fi
-        exit
-    else
-        if [ $callFrom = GUI ] ; then
-            echo '<p class="title">synOCR wurde gestartet ...</p><br><br><br><br>
-            <center><table id="system_msg" style="width: 40%;table-align: center;">
-                <tr>   
-                    <th style="width: 20%;"><img class="imageStyle" alt="status_loading" src="images/status_loading.gif" style="float:left;"></th>   
-                    <th style="width: 80%;"><p class="center"><span style="color: #424242;font-weight:normal;">Bitte warten, bis die Dateien<br>fertig abgearbeitet wurden.</span></p></th>
-                </tr>
-            </table></center>'
-        else
-            echo "synOCR wurde gestartet ..."
-            echo "Bitte warten, bis die Dateien fertig abgearbeitet wurden."
-        fi
-    fi
-
-# ist das Quellverzeichnis vorhanden und ist der Pfad zulässig?
-    if [ ! -d "${INPUTDIR}" ] || ! $(echo "${INPUTDIR}" | grep -q "/volume") ; then
-        if [ $callFrom = GUI ] ; then
-            echo '
-            <p class="center"><span style="color: #BD0010;"><b>! ! ! Quellverzeichnis in der Konfiguration prüfen ! ! !</b><br>Programmlauf wird beendet.<br></span></p>'
-        else
-            echo "! ! ! Quellverzeichnis in der Konfiguration prüfen ! ! !"
-            echo "Programmlauf wird beendet."
-        fi
-        exit 1
-    fi
-
-# muss das Zielverzeichnis erstellt werden und ist der Pfad zulässig?
-    if [ ! -d "$OUTPUTDIR" ] && echo "$OUTPUTDIR" | grep -q "/volume" ; then
-        mkdir -p "$OUTPUTDIR"
-        if [ $callFrom = GUI ] ; then
-            echo '
-            <p class="center"><span style="color: #BD0010;"><b>Zielverzeichnis wurde erstellt.</b></span></p>'
-        else
-            echo "Zielverzeichnis wurde erstellt."
-        fi
-    elif [ ! -d "$OUTPUTDIR" ] || ! $(echo "$BACKUPDIR" | grep -q "/volume") ; then
-        if [ $callFrom = GUI ] ; then
-            echo '
-            <p class="center"><span style="color: #BD0010;"><b>! ! ! Zielverzeichnis in der Konfiguration prüfen ! ! !</b><br>Programmlauf wird beendet.<br></span></p>'
-        else
-            echo "! ! ! Zielverzeichnis in der Konfiguration prüfen ! ! !"
-            echo "Programmlauf wird beendet."
-        fi
-        exit 1
-    fi
-
-# Dateizähler:
-    if [ ! -f ./etc/counter ] ; then
-        touch ./etc/counter
-        echo "startcount=\"$(date +%Y)-$(date +%m)-$(date +%d)\"" >> ./etc/counter
-        echo "ocrcount=\"0\"" >> ./etc/counter
-        echo "pagecount=\"0\"" >> ./etc/counter
-        echo "checkmon=\"\"" >> ./etc/counter
-        echo "                      --> counter-File wurde erstellt"
-    else
-        if ! cat ./etc/counter | grep -q "pagecount" ; then
-            echo "pagecount=\"$(get_key_value ./etc/counter ocrcount)\"" >> ./etc/counter
-        fi
-        if ! cat ./etc/counter | grep -q "checkmon" ; then
-            echo "checkmon=\"\"" >> ./etc/counter
-        fi
-    fi
-    if [[ $(get_key_value ./etc/counter checkmon) != $(date +%m) ]]; then
-        if [[ $(wget --timeout=30 --tries=3 -q -O - "http://geimist.eu/synOCR/VERSION" | head -n1) = "ok" ]]; then
-            synosetkeyvalue ./etc/counter checkmon $(date +%m)
-        fi
-    fi
-
-# nur starten (LOG erstellen), sofern es etwas zu tun gibt:
-    if [ $( ls -t "${INPUTDIR}" | egrep -oi "${SearchPraefix}.*.pdf$" | wc -l ) = 0 ] ;then
-        if [ $callFrom = GUI ] ; then
-            echo '
-            <p class="center"><span style="color: #228b22;"><b>es gibt nichts zu tun</b><br>Programmlauf wird beendet.<br></span></p>'
-        else
-            echo "es gibt nichts zu tun"
-            echo "Programmlauf wird beendet."
-        fi
-        exit 0
-    fi
-
-# synOCR starten und ggf. Logverzeichnis prüfen und erstellen
-    LOGDIR="${LOGDIR%/}/"
-    LOGFILE="${LOGDIR}synOCR_`date +%Y`-`date +%m`-`date +%d`_`date +%H`-`date +%M`.log"
-    touch "$LOGFILE"
+    IFS=$'\012'
+    for entry in $sqlerg; do
+        IFS=$OLDIFS
+        profile_ID=$(echo "$entry" | awk -F'\t' '{print $1}')
+        INPUTDIR=$(echo "$entry" | awk -F'\t' '{print $2}')
+        OUTPUTDIR=$(echo "$entry" | awk -F'\t' '{print $3}')
+        LOGDIR=$(echo "$entry" | awk -F'\t' '{print $4}')
+        SearchPraefix=$(echo "$entry" | awk -F'\t' '{print $5}')
+        loglevel=$(echo "$entry" | awk -F'\t' '{print $6}')
     
-    umask 000   # damit Files auch von anderen Usern bearbeitet werden können / http://openbook.rheinwerk-verlag.de/shell_programmierung/shell_011_003.htm
-
-    if echo "$LOGDIR" | grep -q "/volume" && [ -d "$LOGDIR" ] && [ "$loglevel" != 0 ] ;then   
-        ./synOCR.sh "$LOGFILE" >> $LOGFILE 2>&1     # $LOGFILE wird als Parameter an synOCR übergeben, da die Datei dort ggf. bei ERRORFILES benötigt wird
-    elif echo "$LOGDIR" | grep -q "/volume" && [ ! -d "$LOGDIR" ] && [ "$loglevel" != 0 ] ;then  
-        mkdir -p "$LOGDIR"
-        ./synOCR.sh "$LOGFILE" >> $LOGFILE 2>&1
-    else
-    loglevel=0
-        ./synOCR.sh
-    fi
-
-    if (( $? == 0 )); then
-        echo "    -----------------------------------" >> $LOGFILE
-        echo "    |       ==> synOCR ENDE <==       |" >> $LOGFILE
-        echo "    -----------------------------------" >> $LOGFILE
-    else
-        echo "    -----------------------------------" >> $LOGFILE
-        echo "    |   synOCR mit Fehlern beendet!   |" >> $LOGFILE
-        echo "    -----------------------------------" >> $LOGFILE
-        echo "synOCR wurde mit Fehlern beendet!"
-        echo "weitere Informationen im LOG: $LOGFILE"
-        exit 1
-    fi
-
+    # läuft bereits eine Instanz von synOCR?
+        synOCR_pid=$( pidof synOCR.sh )
+        if [ ! -z "$synOCR_pid" ] ; then
+            if [ $callFrom = GUI ] ; then
+                echo '<p class="center"><span style="color: #BD0010;"><b>synOCR läuft bereits!</b><br>(Prozess-ID: '$synOCR_pid')</span></p>'
+                echo '<br /><p class="center"><button name="page" value="status-kill-synocr" style="color: #BD0010;">(Beenden erzwingen?)</button></p><br />'
+            else
+                echo "synOCR läuft bereits! (Prozess-ID: ${synOCR_pid})"
+            fi
+            exit
+        else
+            if [ $callFrom = GUI ] ; then
+                echo '<p class="title">synOCR wurde gestartet ...</p><br><br><br><br>
+                <center><table id="system_msg" style="width: 40%;table-align: center;">
+                    <tr>   
+                        <th style="width: 20%;"><img class="imageStyle" alt="status_loading" src="images/status_loading.gif" style="float:left;"></th>   
+                        <th style="width: 80%;"><p class="center"><span style="color: #424242;font-weight:normal;">Bitte warten, bis die Dateien<br>fertig abgearbeitet wurden.</span></p></th>
+                    </tr>
+                </table></center>'
+            else
+                echo "synOCR wurde gestartet ..."
+                echo "Bitte warten, bis die Dateien fertig abgearbeitet wurden."
+            fi
+        fi
+    
+    # ist das Quellverzeichnis vorhanden und ist der Pfad zulässig?
+        if [ ! -d "${INPUTDIR}" ] || ! $(echo "${INPUTDIR}" | grep -q "/volume") ; then
+            if [ $callFrom = GUI ] ; then
+                echo '
+                <p class="center"><span style="color: #BD0010;"><b>! ! ! Quellverzeichnis in der Konfiguration prüfen ! ! !</b><br>Programmlauf wird beendet.<br></span></p>'
+            else
+                echo "! ! ! Quellverzeichnis in der Konfiguration prüfen ! ! !"
+                echo "Programmlauf wird beendet."
+            fi
+            exit 1
+        fi
+    
+    # muss das Zielverzeichnis erstellt werden und ist der Pfad zulässig?
+        if [ ! -d "$OUTPUTDIR" ] && echo "$OUTPUTDIR" | grep -q "/volume" ; then
+            mkdir -p "$OUTPUTDIR"
+            if [ $callFrom = GUI ] ; then
+                echo '
+                <p class="center"><span style="color: #BD0010;"><b>Zielverzeichnis wurde erstellt.</b></span></p>'
+            else
+                echo "Zielverzeichnis wurde erstellt."
+            fi
+        elif [ ! -d "$OUTPUTDIR" ] || ! $(echo "$OUTPUTDIR" | grep -q "/volume") ; then
+            if [ $callFrom = GUI ] ; then
+                echo '
+                <p class="center"><span style="color: #BD0010;"><b>! ! ! Zielverzeichnis in der Konfiguration prüfen ! ! !</b><br>Programmlauf wird beendet.<br></span></p>'
+            else
+                echo "! ! ! Zielverzeichnis in der Konfiguration prüfen ! ! !"
+                echo "Programmlauf wird beendet."
+            fi
+            exit 1
+        fi
+    
+    # Dateizähler:
+        if [ ! -f ./etc/counter ] ; then
+            touch ./etc/counter
+            echo "startcount=\"$(date +%Y)-$(date +%m)-$(date +%d)\"" >> ./etc/counter
+            echo "ocrcount=\"0\"" >> ./etc/counter
+            echo "pagecount=\"0\"" >> ./etc/counter
+            echo "checkmon=\"\"" >> ./etc/counter
+            echo "                      --> counter-File wurde erstellt"
+        else
+            if ! cat ./etc/counter | grep -q "pagecount" ; then
+                echo "pagecount=\"$(get_key_value ./etc/counter ocrcount)\"" >> ./etc/counter
+            fi
+            if ! cat ./etc/counter | grep -q "checkmon" ; then
+                echo "checkmon=\"\"" >> ./etc/counter
+            fi
+        fi
+        if [[ $(get_key_value ./etc/counter checkmon) != $(date +%m) ]]; then
+            if [[ $(wget --timeout=30 --tries=3 -q -O - "http://geimist.eu/synOCR/VERSION" | head -n1) = "ok" ]]; then
+                synosetkeyvalue ./etc/counter checkmon $(date +%m)
+            fi
+        fi
+    
+    # nur starten (LOG erstellen), sofern es etwas zu tun gibt:
+        if [ $( ls -t "${INPUTDIR}" | egrep -oi "${SearchPraefix}.*.pdf$" | wc -l ) = 0 ] ;then
+            if [ $callFrom = GUI ] ; then
+                echo '
+                <p class="center"><span style="color: #228b22;"><b>es gibt nichts zu tun</b><br>Programmlauf wird beendet.<br></span></p>'
+            else
+                echo "es gibt nichts zu tun"
+                echo "Programmlauf wird beendet."
+            fi
+            exit 0
+        fi
+    
+    # synOCR starten und ggf. Logverzeichnis prüfen und erstellen
+        LOGDIR="${LOGDIR%/}/"
+        LOGFILE="${LOGDIR}synOCR_`date +%Y`-`date +%m`-`date +%d`_`date +%H`-`date +%M`.log"
+        # touch "$LOGFILE"
+        
+        umask 000   # damit Files auch von anderen Usern bearbeitet werden können / http://openbook.rheinwerk-verlag.de/shell_programmierung/shell_011_003.htm
+    
+        if echo "$LOGDIR" | grep -q "/volume" && [ -d "$LOGDIR" ] && [ "$loglevel" != 0 ] ;then   
+            ./synOCR.sh "$profile_ID" "$LOGFILE" >> $LOGFILE 2>&1     # $LOGFILE wird als Parameter an synOCR übergeben, da die Datei dort ggf. bei ERRORFILES benötigt wird
+        elif echo "$LOGDIR" | grep -q "/volume" && [ ! -d "$LOGDIR" ] && [ "$loglevel" != 0 ] ;then  
+            mkdir -p "$LOGDIR"
+            ./synOCR.sh "$profile_ID" "$LOGFILE" >> $LOGFILE 2>&1
+        else
+        loglevel=0
+            ./synOCR.sh "$profile_ID"
+        fi
+    
+        if (( $? == 0 )); then
+            echo "    -----------------------------------" >> $LOGFILE
+            echo "    |       ==> synOCR ENDE <==       |" >> $LOGFILE
+            echo "    -----------------------------------" >> $LOGFILE
+        else
+            echo "    -----------------------------------" >> $LOGFILE
+            echo "    |   synOCR mit Fehlern beendet!   |" >> $LOGFILE
+            echo "    -----------------------------------" >> $LOGFILE
+            echo "synOCR wurde mit Fehlern beendet!"
+            echo "weitere Informationen im LOG: $LOGFILE"
+            exit 1
+        fi
+    done
+    
 exit 0
