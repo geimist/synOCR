dc408d143c586d5a9277b7600bba3ef24d96dff9
Stephan Geisler <synotr@geimist.eu>
2020-02-02 14:34:23 +0000

    „APP/footer.sh“ ändern

--- APP/footer.sh
+++ APP/footer.sh
@@ -10,20 +10,20 @@ if [[ "$mainpage" == "timer" ]]; then
 		if [[ "$page" == timer-* ]]; then
 			if [[ "$page" == "timer-set-1" ]]; then
 				echo '
-				<button name="page" value="timer">zurück</button>&nbsp;&nbsp;&nbsp;'
+				<button name="page" value="timer">'$lang_buttonback'</button>&nbsp;&nbsp;&nbsp;'
 			elif [[ "$page" == "timer-set-5" ]] && [[ "$timer_times" == "one" ]]; then
 				echo '
-				<button name="page" value="timer-set-2">zurück</button>&nbsp;&nbsp;&nbsp;'
+				<button name="page" value="timer-set-2">'$lang_buttonback'</button>&nbsp;&nbsp;&nbsp;'
 			else
 				echo '
-				<button name="page" value="timer-set-'$siteless'">zurück</button>&nbsp;&nbsp;&nbsp;'
+				<button name="page" value="timer-set-'$siteless'">'$lang_buttonback'</button>&nbsp;&nbsp;&nbsp;'
 			fi
 			if [[ "$page" == "timer-set-5" ]]; then
 				echo '
-				<button name="page" value="timer-set-6"  class="blue_button">Zeitplan speichern</button>'
+				<button name="page" value="timer-set-6"  class="blue_button">'$lang_foot_buttonsavetimer'</button>'
 			else
 				echo '
-				<button name="page" value="timer-set-'$sitemore'">weiter</button>'
+				<button name="page" value="timer-set-'$sitemore'">'$lang_buttonnext'</button>'
 			fi
 		fi
 	fi
@@ -40,11 +40,11 @@ elif [[ "$mainpage" == "edit" ]]; then
 		# button:
 	echo '
 		<div style="text-align: right;">
-	    <button name="page" value="edit-save" class="blue_button">Speichern</button>&nbsp;
-	    <button name="page" value="edit-new_profile-query">neues Profil</button>&nbsp;
-	    <button name="page" value="edit-dup-profile-query" title="erstellt ein neues Profil mit der Konfiguration des aktuellen Profils …">Profil clonen</button>&nbsp; 
-	    <button name="page"><a href="etc/synOCR.sqlite" download="synOCR.sqlite" title="Download der Konfigurationsdatenbank …">Download</a></button>&nbsp;
-	    <button name="page" value="edit-del_profile-query" class="red_button" title="löscht das aktuelle Profil nach Sicherheitsabfrage …">Profil löschen</button>&nbsp;
+	    <button name="page" value="edit-save" class="blue_button">'$lang_buttonsave'</button>&nbsp;
+	    <button name="page" value="edit-new_profile-query">'$lang_foot_buttonnewprofile'</button>&nbsp;
+	    <button name="page" value="edit-dup-profile-query" title="'$lang_foot_buttoncloneprof_tit'">'$lang_foot_buttoncloneprof'</button>&nbsp; 
+	    <button name="page"><a href="etc/synOCR.sqlite" download="synOCR.sqlite" title="'$lang_foot_buttondownDB_tit'">'$lang_foot_buttondownDB'</a></button>&nbsp;
+	    <button name="page" value="edit-del_profile-query" class="red_button" title="'$lang_foot_buttondelprof_tit'">'$lang_foot_buttondelprof'</button>&nbsp;
 	    </div>'
     echo '
         </p>
