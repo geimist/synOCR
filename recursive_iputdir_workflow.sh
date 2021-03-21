26a16b53922c00938a4683dbe7049dec8e13ee71
Stephan Geisler <synotr@geimist.eu>
2021-03-21 21:17:04 +0000

    Dateien hochladen nach „“

---
+++ recursive_iputdir_workflow.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+# /volume3/DEV/SPK_DEVELOPING/synOCR_BUILD/multidir_workflow.sh
+
+SOURCEPARENTDIR=""
+SYNOCR_INPUT=""
+SYNOCR_OUTPUT=""
+
+#-----------------------------------|
+# ab hier nichts mehr ändern        |
+#-----------------------------------|
+
+preprocess() {
+# verschiebe Quelldateien nach SYNOCR_INPUT:
+    IFS=$'\012'
+    for i in $(find "${SOURCEPARENTDIR}" -iname "*.pdf" -type f); do
+        IFS=$OLDIFS
+        FILEPATH=$(dirname "$i")
+        FILENAME=$(basename "$i")
+        ID="$(date +%s%N)_"
+    
+    # erstelle Indexeintrag:
+        echo "${ID}§_§${FILEPATH}§_§${FILENAME}" >> "$INDEXFILE"
+    
+    # verschiebe Quelldatei:
+        mv "$i" "${SYNOCR_INPUT}${ID}${FILENAME}"
+    done
+}
+
+postprocess() {
+# verarbeitete Dateien zurücksortieren:
+    cat "$INDEXFILE" | while read data ; do
+        FILEPATH=$(echo $data | awk -F'§_§' '{print $2}')
+        FILENAME=$(echo $data | awk -F'§_§' '{print $3}')
+        ID=$(echo $data | awk -F'§_§' '{print $1}')
+        
+        FILEHOME="${FILEPATH}/${FILENAME}"
+        OCRFILE=$( find "${SYNOCR_OUTPUT}" -iname "${ID}*.pdf" )
+        mv "$OCRFILE" "$FILEHOME"
+    done
+    
+    mv "$INDEXFILE" "${INDEXFILE}_finish"
+}
+
+OLDIFS=$IFS
+
+APPDIR=$(cd $(dirname $0);pwd)
+
+if [ ! -d "$SOURCEPARENTDIR" ] || [ ! -d "$SYNOCR_INPUT" ] || [ ! -d "$SYNOCR_OUTPUT" ] ; then
+    echo "Pfad ungültig!"
+    exit
+fi
+
+SOURCEPARENTDIR="${SOURCEPARENTDIR%/}/"
+SYNOCR_INPUT="${SYNOCR_INPUT%/}/"
+SYNOCR_OUTPUT="${SYNOCR_OUTPUT%/}/"
+
+INDEXFILE="$(cd $(dirname $0);pwd)/multidir_workflow_INDEX.txt"
+
+if [ ! -f "$INDEXFILE" ] ; then
+    # backup, damit durch erneutes Starten nicht die bisherigen IDs gelöscht werden
+    touch "$INDEXFILE"
+    echo "Index wird erstellt ➜ verschiebe Dateien in den Arbeitsordner"
+    preprocess
+else
+    echo "Index bereits vorhanden ➜ sortiere verarbeitete Dateien zurück"
+    postprocess
+fi
+
