9a7d60ea521a1578dd721a65f4b718d635730def
Stephan Geisler <synotr@geimist.eu>
2021-12-12 13:49:23 +0000

    „APP/ui/synOCR.sh“ ändern

--- APP/ui/synOCR.sh
+++ APP/ui/synOCR.sh
@@ -202,13 +202,7 @@ update_dockerimage()
         updatelog=$(docker pull $dockercontainer)
 
     # purge images:
-        purge_log=$([[ $(docker images -f "dangling=true" -q) ]] && docker rmi -f $(docker images -f "dangling=true" -q))
-    # step 1:
-    #   docker image prune -f
-    # step 2:
-    #   for i in $(docker images --filter "dangling=true" --format "{{.ID}}:{{.Repository}}:{{.Tag}}" | grep "<none>");do
-    #       docker image rm -f $(echo "$i" | awk '-F:' '{print $1}')
-    #   done
+        purge_log=$([[ $(docker images -f "dangling=true" --format "{{.ID}}:{{.Repository}}:{{.Tag}}" | grep "ocrmypdf") ]] && docker rmi -f $(docker images -f "dangling=true" --format "{{.ID}}:{{.Repository}}:{{.Tag}}" | grep "ocrmypdf" | awk -F: '{print $1}'))
 
         if [ -z $(sqlite3 "./etc/synOCR.sqlite"  "SELECT * FROM dockerupdate WHERE image='$dockercontainer'") ]; then
             sqlite3 "./etc/synOCR.sqlite" "INSERT INTO dockerupdate ( image, date_checked ) VALUES  ( '$dockercontainer', '$check_date' )"
