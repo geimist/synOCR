d788ec50e9f52d154150d0c6e8c740a7b1346210
Stephan Geisler <synotr@geimist.eu>
2021-12-25 21:23:56 +0000

    „build_spk.sh“ ändern

--- build_spk.sh
+++ build_spk.sh
@@ -144,6 +144,9 @@ printf "\n - INFO: insert language strings to WIZARD_UIFILES:\n"
     sed -i "s/lang_wizui_uninstall_title/$(get_key_value "$build_tmp/APP/ui/lang/lang_enu.txt" lang_wizui_uninstall_title)/" "$build_tmp/$PKG/WIZARD_UIFILES/uninstall_uifile"
     sed -i "s/lang_wizui_uninstall_desc/$(get_key_value "$build_tmp/APP/ui/lang/lang_enu.txt" lang_wizui_uninstall_desc)/" "$build_tmp/$PKG/WIZARD_UIFILES/uninstall_uifile"
 
+[ -f "$build_tmp/$PKG/WIZARD_UIFILES/upgrade_uifile" ] && echo "vorhanden"
+sed -i "s/lang_wizui_upgrade_title/$(get_key_value "$build_tmp/APP/ui/lang/lang_enu.txt" lang_wizui_upgrade_title)/" "$build_tmp/$PKG/WIZARD_UIFILES/upgrade_uifile"
+exit
     # upgrade_uifile
     [ -f "$build_tmp/$PKG/WIZARD_UIFILES/upgrade_uifile" ] && sed -i "s/lang_wizui_upgrade_title/$(get_key_value "$build_tmp/APP/ui/lang/lang_enu.txt" lang_wizui_upgrade_title)/" "$build_tmp/$PKG/WIZARD_UIFILES/upgrade_uifile"
     [ -f "$build_tmp/$PKG/WIZARD_UIFILES/upgrade_uifile" ] && sed -i "s/lang_wizui_upgrade_desc/$(get_key_value "$build_tmp/APP/ui/lang/lang_enu.txt" lang_wizui_upgrade_desc)/" "$build_tmp/$PKG/WIZARD_UIFILES/upgrade_uifile"
