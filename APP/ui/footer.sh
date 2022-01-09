96413415439bbd84681b85d1cdf088eadbf7116f
Stephan Geisler <synotr@geimist.eu>
2022-01-09 17:14:27 +0000

    „APP/ui/footer.sh“ ändern

--- APP/ui/footer.sh
+++ APP/ui/footer.sh
@@ -36,19 +36,14 @@ if [[ "$mainpage" == "timer" ]]; then
 elif [[ "$mainpage" == "edit" ]]; then
     echo '
         <footer>
-        <p>'
-        # button:
-    echo '
-        <div style="text-align: right;">
-        <button name="page" value="edit-save" class="btn btn-white btn-sm" style="color: #FFFFFF; background-color: green;">'$lang_buttonsave'</button>&nbsp;
-        <button name="page" value="edit-new_profile-query" class="btn btn-warning btn-sm">'$lang_foot_buttonnewprofile'</button>&nbsp;
-        <button name="page" value="edit-dup-profile-query" title="'$lang_foot_buttoncloneprof_tit'" class="btn btn-warning btn-sm">'$lang_foot_buttoncloneprof'</button>&nbsp;
-        <button name="page" value="edit-del_profile-query" title="'$lang_foot_buttondelprof_tit'" class="btn btn-white btn-sm" style="color: #FFFFFF; background-color: #BD0010;">'$lang_foot_buttondelprof'</button>&nbsp;
-        <a href="etc/synOCR.sqlite" download="synOCR.sqlite" title="'$lang_foot_buttondownDB_tit'" class="btn btn-secondary btn-sm">'$lang_foot_buttondownDB'</a>
-        </div>'
-    echo '
-        </p>
+            <div class="fixed-bottom bg-white p-4 border-top border-light border-5">
+                <div style="text-align: right;">
+                    <button name="page" value="edit-save" class="btn btn-white btn-sm" style="color: #FFFFFF; background-color: green;">'$lang_buttonsave'</button>&nbsp;
+                    <button name="page" value="edit-new_profile-query" class="btn btn-warning btn-sm">'$lang_foot_buttonnewprofile'</button>&nbsp;
+                    <button name="page" value="edit-dup-profile-query" title="'$lang_foot_buttoncloneprof_tit'" class="btn btn-warning btn-sm">'$lang_foot_buttoncloneprof'</button>&nbsp;
+                    <button name="page" value="edit-del_profile-query" title="'$lang_foot_buttondelprof_tit'" class="btn btn-white btn-sm" style="color: #FFFFFF; background-color: #BD0010;">'$lang_foot_buttondelprof'</button>&nbsp;
+                    <a href="etc/synOCR.sqlite" download="synOCR.sqlite" title="'$lang_foot_buttondownDB_tit'" class="btn btn-secondary btn-sm">'$lang_foot_buttondownDB'</a>
+                </div>
+            </div>
         </footer>'
-    echo '
-        <div class="clear"></div>'
 fi
