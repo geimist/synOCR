5de3e401f5fc59a57f6fdf4b9a639c89c7a50ded
Stephan Geisler <synotr@geimist.eu>
2022-01-09 08:14:16 +0000

    „APP/ui/main.sh“ ändern

--- APP/ui/main.sh
+++ APP/ui/main.sh
@@ -47,10 +47,12 @@ PATH=$PATH:/usr/local/bin:/opt/usr/bin
 # manual synOCR start:
 # ---------------------------------------------------------------------
     if [[ "$page" == "main-run-synocr" ]]; then
-      echo '
-      <div class="Content_1Col_full">'
-          /usr/syno/synoman/webman/3rdparty/synOCR/synOCR-start.sh GUI
-      echo '<meta http-equiv="refresh" content="2; URL=index.cgi?page=main"></div>'
+        echo '
+        <div class="Content_1Col_full">'
+            /usr/syno/synoman/webman/3rdparty/synOCR/synOCR-start.sh GUI
+            echo '
+            <meta http-equiv="refresh" content="2; URL=index.cgi?page=main">
+        </div>'
     fi
 
 # Force synOCR exit:
@@ -64,100 +66,107 @@ PATH=$PATH:/usr/local/bin:/opt/usr/bin
 # Body:
 # ---------------------------------------------------------------------
 if [[ "$page" == "main" ]] || [[ "$page" == "" ]]; then
-    echo '<div id="Content_1Col">
-        <div class="Content_1Col_full">
-            <br><br><p style="text-align:center"> <span style="color:#BD0010;font-weight:bold;font-size:1.1em; ">'$lang_main_title1'</span> </p>'
+    # -> Headline
+    echo '
+    <h2 class="synocr-text-blue mt-3">synOCR '$lang_page1'</h2>
+    <p>&nbsp;</p>'
+
+    echo '
+    <h5 class="text-center">
+        <strong class="synocr-text-red">'$lang_main_title1'</strong>
+    </h5>'
 
 # check Docker:
     if [ ! $(which docker) ]; then
 #   if ! $(docker --version | grep -q "version") ; then
     # the user synOCR cannot access docker under unknown circumstances, which falsely triggers the error message
-        echo '<p class="center" style="'$synotrred';">'$lang_main_dockerfailed'<br /><br /></p>'
-        echo '<div class="image-right"> </div>
-            <img class="imageStyle"
-            src="images/status_error@geimist.svg"
-            height="120"
-            width="120"
-            style="float:right;padding: 10px">'
+        echo '
+        <p class="text-center synocr-text-red mb-5">'$lang_main_dockerfailed'</p>
+        <div class="float-end">
+            <img src="images/status_error@geimist.svg" height="120" width="120" style="padding: 10px">
+        </div>'
     elif [ $(synogetkeyvalue /etc.defaults/VERSION majorversion) -ge 7 ] && $(! cat /etc/group | grep ^administrators | grep -q synOCR || ! cat /etc/group | grep ^docker: | grep -q synOCR ); then
-        echo '<p class="center" style="'$synotrred';">'$lang_main_permissions_failed'<br /><br /><code>/usr/syno/synoman/webman/3rdparty/synOCR/synOCR-start.sh</code><br /><br /></p>'
-        echo '<div class="image-right"> </div>
-            <img class="imageStyle"
-            src="images/status_error@geimist.svg"
-            height="120"
-            width="120"
-            style="float:right;padding: 10px">'
+        echo '
+        <p class="text-center synocr-text-red">'$lang_main_permissions_failed'
+            <code class="mb-5">/usr/syno/synoman/webman/3rdparty/synOCR/synOCR-start.sh</code>
+        </p>
+        <div class="float-end">
+            <img src="images/status_error@geimist.svg" height="120" width="120" style=";padding: 10px">
+        </div>'
     elif [[ "$count_inputpdf" == 0 ]]; then
-        echo '<div class="image-right"> </div>
-            <img class="imageStyle"
-            src="images/status_green@geimist.svg"
-            height="120"
-            width="120"
-            style="float:right;padding: 10px">'
+        echo '
+        <div class="float-end">
+            <img src="images/status_green@geimist.svg" height="120" width="120" style="padding: 10px">
+        </div>'
     else
-        echo '<div class="image-right"> </div>
-            <img class="imageStyle"
-            src="images/sanduhr_blue@geimist.svg"
-            height="120"
-            width="120"
-            style="float:right;padding: 10px">'
+        echo '
+        <div class="float-end">
+            <img src="images/sanduhr_blue@geimist.svg" height="120" width="120" style="padding: 10px">
+        </div>'
     fi
 
-    echo '<span class="title">'$lang_main_title2':</span>
-        <p style="text-align:left;"> <br><br>
-          '$lang_main_desc1'</p><p>
-          '$lang_main_desc2' <a href="index.cgi?page=edit" style="'$synocrred';">
-          '$lang_main_desc3'</a>
-          '$lang_main_desc4' <a href="index.cgi?page=help" style="'$synocrred';">
-          '$lang_main_desc5'</a>
-          '$lang_main_desc6' <a href="index.cgi?page=timer" style="'$synocrred';">
-          '$lang_main_desc7'</a>
-          '$lang_main_desc8' '$lang_main_desc9
+    echo '
+    <p>&nbsp;</p>
+    <h2 class="synocr-text-blue">'$lang_main_title2':</h2>
+    <p>&nbsp;</p>
+    <p>'$lang_main_desc1'</p>
+    <p>'$lang_main_desc2'</p>
+    <p>&nbsp;</p>'
 
     if [ $(synogetkeyvalue /etc.defaults/VERSION majorversion) -ge 6 ] || (cat /etc/group | grep ^administrators | grep -q synOCR && cat /etc/group | grep ^docker | grep -q synOCR) ; then
-        echo '<br><br><br><p class="center"><button name="page" class="blue_button" value="main-run-synocr">'$lang_main_buttonrun'</button></p><br />'
+        echo '
+        <p class="text-center">
+            <button name="page" class="btn btn-primary" style="background-color: #0086E5;" value="main-run-synocr">'$lang_main_buttonrun'</button>
+        </p><br />'
     fi
 
 # Section Status / Statistics:
-    echo '<fieldset>
-      <hr style="border-style: dashed; size: 1px;">
-      <br />
-      <details><p>
-        <summary>
-            <span class="detailsitem">'$lang_main_statshead':</span>
-        </summary></p>'
+echo '
+<div class="accordion" id="Accordion-01">
+    <div class="accordion-item border-start-0 border-end-0" style="border-style: dashed none dashed none; size: 1px;">
+        <h2 class="accordion-header" id="Heading-01">
+            <button class="accordion-button collapsed bg-white synocr-accordion-glow-off" type="button" data-bs-toggle="collapse" data-bs-target="#Collapse-01" aria-expanded="false" aria-controls="collapseTwo">
+                <span class="synocr-text-blue">'$lang_main_statshead':</span>
+            </button>
+        </h2>
+        <div id="Collapse-01" class="accordion-collapse collapse border-white" aria-labelledby="Heading-01" data-bs-parent="#Accordion-01">
+            <div class="accordion-body">
+                <table class="table table-borderless" style="width: 70%;">
+                    <thead">
+                        <tr>
+                            <th scope="col">'$lang_main_openjobs':</th>
+                            <th scope="col">&nbsp;</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>'
+                            if [[ "$count_inputpdf" == 0 ]]; then
+                                echo '
+                                <td class="synocr-text-blue">'$lang_main_openfilecount':</td>
+                                <td class="synocr-text-green">'$lang_main_alldone'</td>'
+                            else
+                                echo '
+                                <td class="synocr-text-blue">'$lang_main_openfilecount': </td>
+                                <td class="synocr-text-red">'$count_inputpdf'</td>'
+                            fi
+                            echo '
+                        </tr>
+                        <tr>
+                            <td class="synocr-text-blue">'$lang_main_totalsince' '$(sqlite3 ./etc/synOCR.sqlite "SELECT value_1 FROM system WHERE key='count_start_date'")' (PDF / '$lang_main_pages'):</td>
+                            <td class="synocr-text-green">'$(sqlite3 ./etc/synOCR.sqlite "SELECT value_1 FROM system WHERE key='global_ocrcount'")' / '$(sqlite3 ./etc/synOCR.sqlite "SELECT value_1 FROM system WHERE key='global_pagecount'")'</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
 
-    echo '<table style="width: 700px;" >
-        <tr>
-            <th style="width: 1;"></th><th style="width: 250px;"></th><th></th><th style="width: 250px;"></th>
-        </tr>
-        <tr>
-            <td class="td_color" colspan="2"><b>'$lang_main_openjobs':</b></td><td></td><td></td>
-        </tr>'
 
-    if [[ "$count_inputpdf" == 0 ]]; then
-        echo '<tr><td class="td_color"></td><td><span style="color:#0086E5;font-weight:normal; ">'$lang_main_openfilecount': </span></td>
-        <td><span style="color:green;">'$lang_main_alldone'</span></td></tr>'
-    else
-        echo '<tr><td class="td_color"></td><td><span style="color:#0086E5;font-weight:normal; ">'$lang_main_openfilecount': </span></td>
-        <td><span style="color:#BD0010;">'$count_inputpdf'</span></td></tr>'
-    fi
-
-    echo '<tr><td class="td_color" bgcolor=#fff></td><td><span style="color:#0086E5;font-weight:normal; ">'$lang_main_totalsince' '$(sqlite3 ./etc/synOCR.sqlite "SELECT value_1 FROM system WHERE key='count_start_date'")' (PDF / '$lang_main_pages'):</td><td><span style="color:green;">'$(sqlite3 ./etc/synOCR.sqlite "SELECT value_1 FROM system WHERE key='global_ocrcount'")' / '$(sqlite3 ./etc/synOCR.sqlite "SELECT value_1 FROM system WHERE key='global_pagecount'")'</span></td></tr>'
-
-    echo '</table>
         <!-- <p>Hier soll in Zukunft noch eine Statusübersicht / Statistik zu finden sein …<br>
         - https://developers.google.com/chart/interactive/docs/quick_start<br>
         - http://jsfiddle.net/api/post/jquery/1.6/ (http://elycharts.com/examples) </p>
-        <br><div class="tab"><p>'$dbinfo'</p></div>-->
+        <br><div class="tab"><p>'$dbinfo'</p></div>-->'
 
-        </details>
-
-        <br>
-      <hr style="border-style: dashed; size: 1px;">
-        </fieldset>'
-
-    echo '</div></div><br /></p><p style="text-align:center;"><br /><br /></p>'
-    echo '</div></div><div class="clear"></div>'
 fi
 
