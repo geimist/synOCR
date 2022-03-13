685b5ca1c467204b085f291b9d87680ee1f17fc9
Stephan Geisler <synotr@geimist.eu>
2022-03-13 08:09:44 +0000

    „APP/ui/input_monitor.sh“ ändern

--- APP/ui/input_monitor.sh
+++ APP/ui/input_monitor.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
-#  /usr/syno/synoman/webman/3rdparty/synOCR/includes/input_monitor.sh start stop
+#  /usr/syno/synoman/webman/3rdparty/synOCR/input_monitor.sh [start|stop]
 
 monitored_folders="/usr/syno/synoman/webman/3rdparty/synOCR/etc/inotify.list"
 inotify_process_id=$(ps aux | grep -v "grep" | grep -E "inotifywait.*--fromfile.*inotify.list" | awk -F' ' '{print $2}')
 
 
-# inotify-tools currently not included ...
-#   if [ $(uname --machine) = "x86_64" ] && [ ! $(which inotifywait) ]; then
-#       export PATH=$PATH:${0%/*}/bin
-#       export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${0%/*}/lib
-#   elif [ $(uname --machine) = aarch64 ] ; then
-#       export PATH=$PATH:${0%/*}/bin_aarch64
-#       export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:${0%/*}/lib_aarch64
-#   fi
+# create list (array need for tee) with all active log folders:
+# --------------------------------------------------------------
+LOG_DIR_LIST=()
+while read value ; do
+    [ -d "${value%/*}" ] && LOG_DIR_LIST+=( "$value" )
+done <<<"$(sqlite3 /usr/syno/synoman/webman/3rdparty/synOCR/etc/synOCR.sqlite "SELECT LOGDIR FROM config WHERE active='1' AND LOGDIR IS NOT NULL AND NOT LOGDIR=''" 2>/dev/null | sort | uniq | sed -e "s~$~/inotify.log~g")"
+
 
 if [ ! $(which inotifywait) ]; then
     echo "ERROR: inotify-tools are not installed"
+    echo "You can install the SPK from https://synocommunity.com/package/inotify-tools"
     exit 1
 fi
 
@@ -23,15 +23,15 @@ fi
 inotify_start() {
 # start monitoring:
 # --------------------------------------------------------------
-    if [ -z "$inotify_process_id" ];then
-        echo "Monitoring has started ..."
-        inotifywait --fromfile "${monitored_folders}" -e moved_to -e create --monitor --timeout -1 | 
-            while read line ; do 
-                echo "--------------------------- EVENT --------------------------- $(date +%Y-%m-%d_%H-%M-%S) ---------------------------"
-                echo "$line"
-                /usr/syno/synoman/webman/3rdparty/synOCR/synOCR-start.sh
-            done &
-    fi
+    printf "\n---------- START MONITORING ---------- $(date +%Y-%m-%d_%H-%M-%S) ----------\n" | tee -a "${LOG_DIR_LIST[@]}" > /dev/null
+    nohup inotifywait --fromfile "${monitored_folders}" -e moved_to -e create --monitor --timeout -1 | 
+        while read line ; do 
+            printf "\n---------------- EVENT --------------- $(date +%Y-%m-%d_%H-%M-%S) ----------\n"
+            printf "detected event: $line\n"
+            printf "\nsynOCR-start.sh Log:\n"
+            /usr/syno/synoman/webman/3rdparty/synOCR/synOCR-start.sh
+        done | tee -a "${LOG_DIR_LIST[@]}" > /dev/null &
+
     sleep 1
 }
 
@@ -40,38 +40,24 @@ inotify_stop()
 {
 # stop monitoring:
 # --------------------------------------------------------------
-    if [ -n "$inotify_process_id" ];then
-        echo "--------------------- STOP  MONITORING ---------------------- $(date +%Y-%m-%d_%H-%M-%S) ---------------------------"
-        [ -f "${monitored_folders}" ] && rm -f "${monitored_folders}"
-        kill "$inotify_process_id"
-        if [ $? = 0 ]; then
-            echo "Monitoring ended"
-        else
-            echo "ERROR when stopping the monitoring!"
-            exit 1
-        fi
-        sleep 1
+    printf "\n---------- STOP  MONITORING ---------- $(date +%Y-%m-%d_%H-%M-%S) ----------\n" | tee -a "${LOG_DIR_LIST[@]}" > /dev/null
+    [ -f "${monitored_folders}" ] && rm -f "${monitored_folders}"
+    kill $inotify_process_id
+    if [ $? = 0 ]; then
+        echo "Monitoring ended" | tee -a "${LOG_DIR_LIST[@]}" > /dev/null
+    else
+        echo "ERROR when stopping the monitoring!" | tee -a "${LOG_DIR_LIST[@]}" > /dev/null
+        exit 1
     fi
+
+    sleep 1
 }
 
 
 # start-stop-monitoring:
 case "$1" in
     start)
-        # Check if a restart of the monitoring is necessary / update watch folder list:
-        sqlite3 /usr/syno/synoman/webman/3rdparty/synOCR/etc/synOCR.sqlite "SELECT INPUTDIR FROM config WHERE active='1'" 2>/dev/null | sort | uniq > "${monitored_folders}_tmp"
-        if [ "$(cat "$monitored_folders" 2>/dev/null)" = "$(cat "${monitored_folders}_tmp")" ]; then
-            inotify_start
-            rm -f "${monitored_folders}_tmp"
-        else
-            echo "--------------------- START MONITORING ---------------------- $(date +%Y-%m-%d_%H-%M-%S) ---------------------------"
-            echo "Change noticed in the watched folders!"
-            echo "start / restart monitoring ..."
-            mv "${monitored_folders}_tmp" "${monitored_folders}"
-            inotify_stop
-            sleep 1
-            inotify_start
-        fi
+        inotify_start
         exit 0
         ;;
     stop)
@@ -81,48 +67,3 @@ case "$1" in
 esac
 
 exit 0
-
-
-
-
-
-
-
-
-
-
-
-https://www.linux-community.de/ausgaben/linuxuser/2017/01/bescheid/
-
---fromfile Datei 
-    definiert die zu überwachenden Dateien über eine Datei mit den Pfaden. 
-
---format <fmt>	
-Legt das Ausgabeformat fest. Es werden nur maximal ca. 4000 Zeichen ausgegeben.
-    %w  wird durch den Verzeichnispfad ersetzt
-    %f  wird durch den Dateinamen ersetzt, falls einer relevant ist
-    %e  wird durch die Events ersetzt, mehrere Events werden durch Komma getrennt
-    %Xe wird durch die Events ersetzt, mehrere Events werden durch das Zeichen an der Stelle X ersetzt
-    %T  wird durch die aktuelle Zeit ersetzt, das Format kann mit --timefmt festgelegt werden
-
-Inotify-Events:
-Kürzel          Format
-------------------------------------------------------------
-access          Eine Datei wurde gelesen.
-modify          Eine Datei wurde geändert.
-attrib          Die Metadaten einer Datei wurden geändert (Zeitstempel, Rechte, erweiterte Attribute).
-close_write     Eine Datei wurde geschlossen nachdem sie zum schreiben geöffnet wurde, sie muss aber nicht verändert worden sein.
-close_nowrite   Eine Datei wurde geschlossen nachdem sie schreibgeschützt geöffnet wurde.
-close           wie close_write und close_nowrite zusammen
-open            Eine Datei wurde geöffnet.
-moved_to        Eine Datei oder ein Verzeichnis wurde in ein zu überwachendes Verzeichnis verschoben oder im Verzeichnis verschoben.
-moved_from      Eine Datei oder ein Verzeichnis wurde aus oder in einem überwachten Verzeichnis verschoben.
-move            wie moved_to und moved_from zusammen
-move_self       Eine überwachte Datei oder ein überwachtes Verzeichnis wurde verschoben. Danach wird die Überwachung abgeschaltet.
-create          Eine Datei wurde erstellt.
-delete          Eine Datei wurde gelöscht.
-delete_self     Eine überwachte Datei oder ein überwachtes Verzeichnis wurde gelöscht. Danach wird die Überwachung abgeschaltet.
-unmount         Das Dateisystem, auf dem sich die Überwachung befand, wurde ausgehängt. Danach wird die Überwachung abgeschaltet. Dieses Event kann auch auftreten, wenn es nicht explizit überwacht wurde.
-
-
-
