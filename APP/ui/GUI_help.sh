b45a74ed0645d434de6bb2b8cada0bd57593624f
Stephan <synotr@geimist.eu>
2023-03-03 13:11:16 +0000

    „APP/ui/GUI_help.sh“ ändern

--- APP/ui/GUI_help.sh
+++ APP/ui/GUI_help.sh
@@ -105,10 +105,21 @@ echo '
     <p>
         '$lang_help_about_1'<br />
         '$lang_help_about_2'<br />
-        '$lang_help_about_3'
+        '$lang_help_about_3'<br />
         <a href="https://www.paypal.me/geimist" onclick="window.open(this.href); return false;">
-            <img src="images/paypal.png" alt="PayPal" style="float:right;padding:10px" height="60" width="200"/>
-        </a><br /><br /><hr>
+            <img src="images/paypal.png" alt="PayPal" style="float:left;padding:10px" height="60" width="200"/>
+        </a><br /><br /><br />
+    </p>
+    <p>
+        <a href="https://de.igraal.com/einladung?werber=AG_633f0f4d6b7be" onclick="window.open(this.href); return false;" style="'$synocrred';">'$lang_help_igraal_1' </a>
+        '$lang_help_igraal_2'<br />
+    
+        <a href="https://de.igraal.com/einladung?werber=AG_633f0f4d6b7be" onclick="window.open(this.href); return false;">
+            <img src="images/iGraal.png" alt="iGraal" style="float:left;padding:10px" height="60" width="200"/>
+        </a><br /><br /><br />
+    </p>
+    <p>
+        <hr>
         <p><b>
             '$lang_help_about_4':</b>
             <ul>
